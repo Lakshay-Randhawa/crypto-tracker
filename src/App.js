7a0e284bc8bbbf70af4cc0159b91716601607f11
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2022-07-12 19:30:31 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+import Coin from "./Coin";
+import './App.css'
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false
 function App() {
+  
+ // States
+ const [coins, setCoin] = useState([])
+ const [search, editSearch] = useState('b')
+
+    useEffect(()=>{
+
+      //Fetching data from coingecko API
+      axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res =>{
+        setCoin(res.data)
+      })
+      .catch(error => console.log(error))
+    },[])  
    
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-         .then(res =>{
-          console.log(res.data)
-         })
+    const handleChange = e =>{
+       editSearch(e.target.value)
+    }
+
+    const filteredCoins = coins.filter(coin =>
+      coin.name.toLowerCase().includes(search.toLowerCase())
+    );
+
   return (
-    <div>
-        <h1>This is api app</h1>
+    <div className="coin-app">
+        <div className="coin-search">
+          <h1 className="coin-text">Search a currency</h1>
+          <input className="coin-input" type='text' placeholder="Search" onChange={handleChange} />
+        </div>
+        {filteredCoins.map(coin => (
+          <Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image} 
+          symbol={coin.symbol} 
+          marketcap={coin.market_cap}
+          price={coin.current_price} 
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}
+          />
+          
+        ))}
     </div>
   );
 }
